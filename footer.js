90f3fb07cc988a21bafa4c6766950a1b3467df9d
Longfei Li <longfei.li86@gmail.com>
2020-12-27 02:59:15 -0600

    update

---
+++ footer.js
@@ -0,0 +1,7 @@
+document.write('\
+    <footer>\
+<p>\
+  Copyright &copy; Longfei Li |  <a href="http://www.css3templates.co.uk">design from css3templates.co.uk</a>\
+</P>\
+</footer>\
+');
---
+++ sidebar.js
@@ -0,0 +1,29 @@
+document.write('\
+    <div id="sidebar_container">       \
+  <div class="sidebar">\
+</br></br>\
+		<img src="images/Li-Longfei-web.jpg" width="138"  height="206" alt="image1" />\
+	 \
+		<h2>Contact</h2>\
+		<p >\
+		    433 Maxim Doucet Hall<br>\
+		    Department of Mathematics<br>\
+		    University of Louisiana at Lafayette<br>\
+		    Lafayette, LA 70503<br>\
+		    E-Mail: longfei.li@louisiana.edu<br>\
+		    Office Phone: 337-482-5287<br>\
+                                  </br>\
+		    		   <a href="files/CV.pdf">    Curriculum Vitae </a></br>\
+		    <a href="https://scholar.google.com/citations?user=jxp2H9IAAAAJ&hl=en">  Google Scholar  </a> </br >\
+		</p>\
+		<h2>Useful Links</h2>\
+	 <p >			   \
+	   <a href="http://www.overtureframework.org/">  OvertureFramework  </a></br>\
+	   <a href="https://mfem.org"> MFEM  </a></br>\
+	   <a href="https://math.louisiana.edu/">  Math  Department, UL  </a></br>\
+	 </p>\
+		\
+  </div><!--close sidebar-->\
+  \
+</div><!--close sidebar_container-->\
+');
