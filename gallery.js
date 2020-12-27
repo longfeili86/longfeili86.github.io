93a38efa229db26e938edb3ae4065a10603f54b1
Longfei Li <longfei.li86@gmail.com>
2020-12-27 03:22:13 -0600

    update

--- gallery.js
+++ gallery.js
@@ -1,10 +1,10 @@
 document.write('\
     <div class="gallery"> \
     <ul class="images"> \
-    <li class="show"><img width="950" height="300" src="images/4bicCFLs100G16t2p5_VORTICITY.pdf" alt="simplistic 1" /></li> \
-    <li><img width="950" height="300" src="images/plates.pdf" alt="simplistic 2" /></li> \
-    <li><img width="950" height="300" src="images/testFPCP4Streamline.pdf" alt="simplistic 3" /></li> \
-    <li><img width="950" height="300" src="images/OsmFluVarDiffusivityVPermE20New_c_t15.pdf" alt="simplistic 4" /></li> \
+    <li class="show"><img width="950" height="300" src="images/4bicCFLs100G16t2p5_VORTICITY.png" alt="simplistic 1" /></li> \
+    <li><img width="950" height="300" src="images/plates.png" alt="simplistic 2" /></li> \
+    <li><img width="950" height="300" src="images/testFPCP4Streamline.png" alt="simplistic 3" /></li> \
+    <li><img width="950" height="300" src="images/OsmFluVarDiffusivityVPermE20New_c_t15.png" alt="simplistic 4" /></li> \
     </ul> \
     </div> \
 ');
